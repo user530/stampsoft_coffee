e790f7bd38531c5f6ec906b199aeec5e09b463b7
user530 <m.timarsuev@gmail.com>
2024-04-02 22:26:31 +0300

    Added static images

---
+++ src/static/index.js
@@ -0,0 +1,15 @@
+import bean1 from './bean1.png';
+import bean2 from './bean2.png';
+import bean3 from './bean3.png';
+import bean4 from './bean4.png';
+import bean5 from './bean5.png';
+import cup from './cup.png';
+
+export {
+    bean1, 
+    bean2,
+    bean3,
+    bean4,
+    bean5,
+    cup,
+}
