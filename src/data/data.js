a4c6d812e535926102afbfbd3595e3a08ce57f43
user530 <m.timarsuev@gmail.com>
2024-04-03 23:28:32 +0300

    Added products data

---
+++ src/data/data.js
@@ -0,0 +1,174 @@
+export const productData = [
+    {
+        id: "1",
+        name: "Кофе",
+        img: require("../static/categories/coffee.png"),
+        products: [
+            {
+                id: "1a",
+                name: "Экспрессо",
+                img: require("../static/products/coffee/espresso.png"),
+                starterPrice: 79
+            },
+            {
+                id: "1b",
+                name: "Экспрессо 2х",
+                img: require("../static/products/coffee/espresso2x.png"),
+                starterPrice: 109
+            },
+            {
+                id: "1c",
+                name: "Американо",
+                img: require("../static/products/coffee/amerikano.png"),
+                starterPrice: 129
+            },
+            {
+                id: "1d",
+                name: "Латте",
+                img: require("../static/products/coffee/latte.png"),
+                starterPrice: 129
+            },
+            {
+                id: "1e",
+                name: "Капучино",
+                img: require("../static/products/coffee/capuchino.png"),
+                starterPrice: 129
+            },
+            {
+                id: "1f",
+                name: "Макиато",
+                img: require("../static/products/coffee/makiato.png"),
+                starterPrice: 129
+            }
+        ]
+    },
+    {
+        id: "2",
+        name: "Чай",
+        img: require("../static/categories/tea.png"),
+        products: [
+            {
+                id: "2a",
+                name: "Экспрессо",
+                img: require("../static/products/coffee/espresso.png"),
+                starterPrice: 79
+            },
+            {
+                id: "2b",
+                name: "Экспрессо 2х",
+                img: require("../static/products/coffee/espresso2x.png"),
+                starterPrice: 109
+            },
+            {
+                id: "2c",
+                name: "Американо",
+                img: require("../static/products/coffee/amerikano.png"),
+                starterPrice: 129
+            },
+            {
+                id: "2d",
+                name: "Латте",
+                img: require("../static/products/coffee/latte.png"),
+                starterPrice: 129
+            },
+            {
+                id: "2e",
+                name: "Капучино",
+                img: require("../static/products/coffee/capuchino.png"),
+                starterPrice: 129
+            },
+            {
+                id: "2f",
+                name: "Макиато",
+                img: require("../static/products/coffee/makiato.png"),
+                starterPrice: 129
+            }
+        ]
+    },
+    {
+        id: "3",
+        name: "Молочный коктейль",
+        img: require("../static/categories/coctail.png"),
+        products: [
+            {
+                id: "3a",
+                name: "Экспрессо",
+                img: require("../static/products/coffee/espresso.png"),
+                starterPrice: 79
+            },
+            {
+                id: "3b",
+                name: "Экспрессо 2х",
+                img: require("../static/products/coffee/espresso2x.png"),
+                starterPrice: 109
+            },
+            {
+                id: "3c",
+                name: "Американо",
+                img: require("../static/products/coffee/amerikano.png"),
+                starterPrice: 129
+            },
+            {
+                id: "3d",
+                name: "Латте",
+                img: require("../static/products/coffee/latte.png"),
+                starterPrice: 129
+            },
+            {
+                id: "3e",
+                name: "Капучино",
+                img: require("../static/products/coffee/capuchino.png"),
+                starterPrice: 129
+            },
+            {
+                id: "3f",
+                name: "Макиато",
+                img: require("../static/products/coffee/makiato.png"),
+                starterPrice: 129
+            }
+        ]
+    },
+    {
+        id: "4",
+        name: "Морсы и газ.напитки",
+        img: require("../static/categories/juice.png"),
+        products: [
+            {
+                id: "4a",
+                name: "Экспрессо",
+                img: require("../static/products/coffee/espresso.png"),
+                starterPrice: 79
+            },
+            {
+                id: "4b",
+                name: "Экспрессо 2х",
+                img: require("../static/products/coffee/espresso2x.png"),
+                starterPrice: 109
+            },
+            {
+                id: "4c",
+                name: "Американо",
+                img: require("../static/products/coffee/amerikano.png"),
+                starterPrice: 129
+            },
+            {
+                id: "4d",
+                name: "Латте",
+                img: require("../static/products/coffee/latte.png"),
+                starterPrice: 129
+            },
+            {
+                id: "4e",
+                name: "Капучино",
+                img: require("../static/products/coffee/capuchino.png"),
+                starterPrice: 129
+            },
+            {
+                id: "4f",
+                name: "Макиато",
+                img: require("../static/products/coffee/makiato.png"),
+                starterPrice: 129
+            }
+        ]
+    }
+]
