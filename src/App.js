aeedabd0c6c548fb7390723a1579782a46216d3c
user530 <m.timarsuev@gmail.com>
2024-04-04 13:06:01 +0300

    App page added global state for the data and selection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
+import React from 'react';
 import styles from './App.module.scss';
+import { PayCard } from './components/payCard/PayCard';
+import { PayCash } from './components/payCash/PayCash';
 import { Products } from './components/products/Products';
 import { Promo } from './components/promo/Promo';
-import React from 'react';
+import { productData } from './data/data';
 
 function App() {
-  const [appState, setAppState] = React.useState('');
+  const [appState, setAppState] = React.useState('promo');
+  const [products, setProducts] = React.useState([]);
+  const [selectedProduct, setSelectedProduct] = React.useState(null);
 
+  const toPromo = () => setAppState('promo');
   const toProducts = () => setAppState('products');
+  const toPayment = (type) => type === 'card' ? setAppState('payCard') : setAppState('payCash'); 
+
+  React.useEffect(
+    () => setProducts(productData),
+    []
+  )
   
   return (
     <main className={styles['app-wrapper']}>
       { 
-        appState === '' 
+        appState === 'promo' 
         && <Promo headingTxt={'Это твой кофе'} btnText={'Коснитесь экрана'} next={toProducts}/>
       }
 
       {
         appState === 'products' 
-        && <Products/>
+        && <Products productData={products} next={toPayment} prev={toPromo} setSelectedProduct={setSelectedProduct} />
+      }
+
+      {
+        appState === 'payCard' 
+        && <PayCard />
+      }
+
+      {
+        appState === 'payCash' 
+        && <PayCash />
       }
 
     </main>
