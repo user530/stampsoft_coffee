c25e82590d059e6f758596017e84eedbc238dead
user530 <m.timarsuev@gmail.com>
2024-04-05 10:49:20 +0300

    Popup component implemented

---
+++ src/components/products/popup/Popup.jsx
@@ -0,0 +1,31 @@
+import styles from './Popup.module.scss';
+import { VscChromeClose } from "react-icons/vsc";
+
+export const Popup = (props) => {
+    const { children, price, isOpen, closeCb, nextCb } = props;
+
+    return (
+        <section 
+        className={`${styles['overlay']} ${isOpen ? styles['active']: ''}`}
+        >
+            <div className={styles['popup']}>
+                <button 
+                className={styles['popup__close']}
+                onClick={closeCb}
+                >
+                    <VscChromeClose className={styles['popup__close-icon']}/>
+                </button>
+
+                { children }
+
+                <button 
+                className={styles['popup__next']}
+                onClick={nextCb}
+                >
+                    <p>Оплатить</p>
+                    <p className={styles['accent']}>{ price }&#8381;</p>
+                </button>
+            </div>
+        </section>
+    )
+}
