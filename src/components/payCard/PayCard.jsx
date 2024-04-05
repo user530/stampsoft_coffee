941627e86e52f143d52b1d4b901bacb6c87a365f
user530 <m.timarsuev@gmail.com>
2024-04-05 17:58:45 +0300

    PayCard component implemented

--- src/components/payCard/PayCard.jsx
+++ src/components/payCard/PayCard.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import styles from './PayCard.module.scss';
 import sprite from '../../static/sprite.svg';
+import { PinPad } from './pinPad/PinPad';
 
 export const PayCard = (props) => {
     const { next, prev } = props;
+    
+    const cardInputHandler = (success) => { 
+        if(showPinpad) return;
+        
+        console.log('Card input handler');
+        setShowPinpad(true);
+    };
 
-    const cardInputHandler = () => { next(); };
     const clickHandler = () => { prev(); };
+
+    const keyPressHandler = (e) => {
+        if(e.key === '1') 
+            cardInputHandler(true);
+        else if(e.key === '2')
+            cardInputHandler(false);
+    }
+    
+    const [showPinpad, setShowPinpad] = React.useState(false);
+    const closePinpad = React.useCallback(() => setShowPinpad(false), []);
+
+    const submitPin = React.useCallback((pincode) => {console.log('Pincode: ' + pincode)});
+
+    React.useEffect(
+        () => {
+            window.addEventListener('keydown', keyPressHandler);
+
+            return () => {
+                window.removeEventListener('keydown', keyPressHandler);
+            }
+        },
+        []
+    )
     
     return (
         <section className={styles['wrapper']}>
@@ -20,6 +50,8 @@ export const PayCard = (props) => {
             </div>
 
             <button onClick={clickHandler}>Отмена</button>
+
+            <PinPad isActive={showPinpad} closeCb={closePinpad} sumbitCb={submitPin}/>
         </section>
     )
 }
