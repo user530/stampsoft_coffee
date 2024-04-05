d7799ea6bd1a120ddf5c1d41dbc1a6dca6d09847
user530 <m.timarsuev@gmail.com>
2024-04-05 13:18:49 +0300

    Pay card component implemented

--- src/components/payCard/PayCard.jsx
+++ src/components/payCard/PayCard.jsx
@@ -1,7 +1,25 @@
-export const PayCard = () => {
+import React from 'react';
+import styles from './PayCard.module.scss';
+import sprite from '../../static/sprite.svg';
+
+export const PayCard = (props) => {
+    const { next, prev } = props;
+
+    const cardInputHandler = () => { next(); };
+    const clickHandler = () => { prev(); };
+    
     return (
-        <section>
-            <h1>Pay Card</h1>
+        <section className={styles['wrapper']}>
+            <div className={styles['content']}>
+                <div className={styles['content__img']}>
+                    <svg>
+                        <use href={`${sprite}#card_symbol`}/>
+                    </svg>
+                </div>
+                <h3 className={styles['content__title']}>Приложите карту к&nbsp;терминалу</h3>
+            </div>
+
+            <button onClick={clickHandler}>Отмена</button>
         </section>
     )
 }
