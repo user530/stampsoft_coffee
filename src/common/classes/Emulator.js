a0a16a8fa19a6bc4b36cc794b823d2b1de0d3d09
user530 <m.timarsuev@gmail.com>
2024-04-09 19:20:32 +0300

    Emulator Card purchase + EmitCardIn

--- src/common/classes/Emulator.js
+++ src/common/classes/Emulator.js
@@ -1,8 +1,11 @@
 export class Emulator {
     #status = 'READY';
     #callbacks = {};
-    #events = ['cashIn', 'confirmPayment', 'cancelPayment'];
+    #events = ['cashIn', 'cardIn', 'confirmPayment', 'cancelPayment', 'paymentStatusUpdated'];
     #stash = 0;
+    #cardInserted = false;
+    #interupted = false;
+    #chargedAmount = 0; 
 
     get status() {
         return this.#status;
@@ -16,8 +19,18 @@ export class Emulator {
         return this.#stash;
     }
 
-    set stash(newAmount) {
-        this.#stash = newAmount;
+    get cardInserted() {
+        return this.#cardInserted;
+    }
+
+    insertCard = () => {
+        this.#cardInserted = true;
+        console.log('Card inserted!');
+    }
+    
+    ejectCard = () => {
+        this.#cardInserted = false;
+        console.log('Card ejected!');
     }
 
     incrementStash = (addToStash) => {
@@ -36,8 +49,32 @@ export class Emulator {
         this.#status = 'WAITING_CASH';
     }
 
+    setStatusWaitingCard = () => {
+        this.#status = 'WAITING_CARD';
+    }
+
     setStatusWaitingConfirm = () => {
         this.#status = 'WAITING_CONFIRM';
+    }
+
+    setStatusProcessingCard = () => {
+        this.#status = 'PROCESSING_CARD';
+    }
+
+    clearInterupt = () => {
+        this.#interupted = false;
+    }
+
+    interuptOperation = () => {
+        this.#interupted = true;
+    }
+
+    chargeAmount = (amount) => {
+        this.#chargedAmount = amount;
+    }
+
+    resetCharged = () => {
+        this.#chargedAmount = 0;
     }
 
     isValidEvent = (eventName) => {
@@ -94,7 +131,7 @@ export class Emulator {
             return;
 
         // Check argument 
-        if(typeof cashAmount !== 'number' || isNaN(cashAmount)) 
+        if( !cashAmount || typeof cashAmount !== 'number' || isNaN(cashAmount)) 
             return console.error('Provide valid cash amount!');
 
         // Placeholder cashin event, later change to a creator method
@@ -197,6 +234,101 @@ export class Emulator {
         console.log(this);
     }
 
+    BankCardPurchase = (amount, cb, display_cb, confirmCb, cancelCb) => {
+        /* Skip, if emulator is already busy with some other task  */ 
+        if(this.#status !== 'READY') 
+            return;
+        
+        // Update the status
+        this.setStatusWaitingCard();
+
+        // Charge purchase amount
+        this.chargeAmount(amount);
+        
+        // Register callback
+        this.registerCallback('cardIn', (success, reason = '') => cb(success, reason));
+        this.registerCallback('paymentStatusUpdated', (status) => display_cb(status));
+        this.registerCallback('confirmPayment', (result) => confirmCb(result));
+        this.registerCallback('cancelPayment', (reason) => cancelCb(reason));
+    }
+
+    BankCardCancel = () => {
+        // Clear callbacks, clear interupt flag, clear charged amount, eject the card
+    }
+
+    EmitCardIn = async (cardData) => {
+        console.log('Emit cardIn fired');
+        /* Skip, if emulator is already busy with some other task  */ 
+        if(this.#status !== 'WAITING_CARD') 
+            return;
+
+        // Placeholder function that validates data existence and format
+        const argumentsCorrect = (cardData) => true;
+        
+        // Check argument 
+        if( !cardData || !argumentsCorrect(cardData)) 
+            return console.error('Provide valid card data!');
+
+        // Set status
+        this.setStatusProcessingCard();
+
+        // Set card flag
+        this.insertCard();
+
+        // Initial status change before the first check
+        this.EmitStatusUpdate('Обработка карты');
+        
+        try {
+            // Emulate 1st check
+            const check1 = await this.emulateCheck(true);
+            if(!check1) throw new Error('Не удалось обработать карту!');
+            
+            this.EmitStatusUpdate('Связь с банком');
+
+            // Emulate 2nd check
+            const check2 = await this.emulateCheck(false);
+            if(!check2) throw new Error('Не удалось связаться с банком!');
+            
+            this.EmitStatusUpdate('Проверка данных');
+
+            // Emulate 3rd check
+            const check3 = await this.emulateCheck(false);
+            if(!check3) throw new Error('Неверные данные карты!');
+
+            // Emulate amount check -> Here we pass amount to the bank to confirm
+            const check4 = await this.emulateCheck(false, this.#chargedAmount);
+            if(!check4) throw new Error('Недостаточно средств на карте!');
+
+            this.EmitStatusUpdate('Подтвердите транзакцию');
+            
+            // If interuption flag was raised
+            if(this.#interupted) throw new Error('Операция отменена пользователем!');
+            
+            this.setStatusWaitingConfirm();
+
+            // Fire a callback for the event
+            this.#callbacks['cardIn'](true);
+
+            console.log(this);
+
+        } catch (error) {
+            this.setStatusReady();
+            this.ejectCard();
+            this.clearInterupt();
+
+            return this.#callbacks['cardIn'](false, 'Не удалось обработать карту!');
+        }
+    }
+
+    EmitStatusUpdate = (newStatus) => {
+        if(this.#status !== 'PROCESSING_CARD') return;
+
+        if(!newStatus)
+            return console.error('Provide new status!');
+
+        this.#callbacks['paymentStatusUpdated'](newStatus);
+    }
+
     /**
      * Placeholder emulating the return of change
      * @param {Number} amount Amount of money to return 
@@ -212,4 +344,17 @@ export class Emulator {
 
         return emulateConfirm;
     }
+
+    /**
+     * Emulate the check of validity of some data
+     * @param {Boolean} isValid Emulated result that will be returned in the specified time 
+     * @param {Number} timer Time in seconds to wait before the return 
+     * @returns {Promise<Boolean>} Promise of the emulated result 
+     */
+    emulateCheck = async (isValid, timer = 2000) => new Promise(
+            (resolve) => setTimeout(
+                () => resolve(isValid),
+                timer + Math.random() * 1000
+            )
+        );
 }
